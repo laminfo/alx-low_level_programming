61917458baa39e85f369ae9080c0945ec8b1be1b
ELAMMARI Lamia <el.lamiaa123@gmail.com>
2023-03-17 06:57:17 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /*
  * main - this is the main function
  * puts diplay and writ string
  *  return: 0 when successfull
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
