67c5fa5fbfc3486f47756316a427b0967fc54356
ELAMMARI Lamia <el.lamiaa123@gmail.com>
2023-03-17 06:42:00 -0700

    4. Hello, puts

---
+++ 4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main ()for execute program de puts
+ *
+ * returen 0 ilways if condition is realize
+ *
+ */
+ int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
