2e0b878fa754f72a0ebb04d510139816a580d08a
ELAMMARI Lamia <el.lamiaa123@gmail.com>
2023-03-18 11:27:29 -0700

    9. Patience, persistence

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main is function main for execute
+ *
+ * retrun : always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
